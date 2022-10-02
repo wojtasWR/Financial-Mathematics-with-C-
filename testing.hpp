671010c67dedd1f6e6a136747ac92f7eeae6b5b5
wojtasWR <63214033+wojtasWR@users.noreply.github.com>
2022-10-02 21:32:52 +0200

    Add files via upload

---
+++ testing.hpp
@@ -0,0 +1,13 @@
+//
+//  testing.hpp
+//  finance
+//
+//  Created by Wojciech Ratusznik on 2022-10-02.
+//
+
+#ifndef testing_hpp
+#define testing_hpp
+
+#include <stdio.h>
+
+#endif /* testing_hpp */
