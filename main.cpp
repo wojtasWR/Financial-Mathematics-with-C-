707cbc06f011e92ac159c94843becb348ca877bb
wojtasWR <63214033+wojtasWR@users.noreply.github.com>
2023-02-26 22:39:54 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "matlib.h"
+#include "geometry.h"
+#include "BlackScholesModel.h"
+/*
+#include "textfunctions.h"
+#include "CallOption.h"
+#include "PutOption.h"
+#include "PieChart.h"
+#include "LineChart.h"
+#include "BlackScholesModel.h"
+#include "Histogram.h"
+#include "MonteCarloPricer.h"
+#include "UpAndOutOption.h"
+#include "DownAndOutOption.h"
+#include "Portfolio.h"
+#include "DigitalCallOption.h"
+#include "DigitalPutOption.h"
+#include "RectangleRulePricer.h"
+ */
+#include "Simulator2.h"
+
+using namespace std;
+
+int main() {
+
+   
+    testBSMsimulator();
+    
+    
+    
+    return 0;
+}
