955237d61c35dc0bd27d21b9651c22c4714503b2
wojtasWR <63214033+wojtasWR@users.noreply.github.com>
2023-02-26 23:16:45 +0100

    Add files via upload

--- Simulator2.cpp
+++ Simulator2.cpp
@@ -1,11 +1,3 @@
-//
-//  Simulator2.cpp
-//  FMLib
-//
-//  Created by Wojciech Ratusznik on 2023-02-21.
-//  Copyright © 2023 C++ for financial mathematics. All rights reserved.
-//
-
 #include <stdio.h>
 
 #include "matlib.h"
@@ -14,6 +6,7 @@
 #include <iterator>
 #include <algorithm>
 #include <functional>
+#include <chrono>
 
 using namespace std;
 
@@ -34,7 +27,7 @@ vector<double> Simulator2::indexSimulation(const BlackScholesModel& bsm, vector<
     {
         temp_res = bsm.generateRiskNeutralPricePath(dates,
                                                     randN[i]);
-        for (int j = 0; j < nDates; j++)
+        for (int j = 0; j < nDates; j++)//There should be a better way to add two vectors, something like transform. 
         {
             res[j] += temp_res[j]/nScenarios;
         }
@@ -43,7 +36,7 @@ vector<double> Simulator2::indexSimulation(const BlackScholesModel& bsm, vector<
     return res;
 }
 
-vector<vector<double>> Simulator2::simulation(vector<double> dates)
+vector<vector<double>> Simulator2::simulation(vector<double> dates, bool antithetic, bool standardisation)
 {
     rng("default");
     
@@ -51,46 +44,60 @@ vector<vector<double>> Simulator2::simulation(vector<double> dates)
     
     int nDates = dates.size();
     
-    for(int i = 0; i < nScenarios / 2; i++) //lägger på antithetic + normalisering snart. ska få det att fungera först,
+    int anti = 1; //dåligt namn.
+    if(antithetic)
+    {
+        anti = 2;
+    }
+    
+    for(int i = 0; i < nScenarios / anti; i++)
     {
         vector<double> unnormalized_randn = randn(nDates);
-        vector<double> normalized_randn(nDates,0.0);
         
-         //Normalization so that we really get the N(0,1)
-        double normalized_mean = mean(unnormalized_randn);
-        double normalized_std = standardDeviation(unnormalized_randn, true);
-        vector<double> myconstants = {normalized_mean,normalized_std};
-        
-        transform(unnormalized_randn.begin(), unnormalized_randn.end(), normalized_randn.begin(), [&myconstants](double& c){return (c-myconstants[0]) / myconstants[1] ;});
-        
-        randNumbers.push_back(normalized_randn);
+        //Normalization so that we really get the N(0,1)
+        if(standardisation)
+        {
+            vector<double> normalized_randn(nDates,0.0);
+            double normalized_mean = mean(unnormalized_randn);
+            double normalized_std = standardDeviation(unnormalized_randn, true);
+            vector<double> myconstants = {normalized_mean,normalized_std};
+            
+            transform(unnormalized_randn.begin(), unnormalized_randn.end(), normalized_randn.begin(), [&myconstants](double& c){return (c-myconstants[0]) / myconstants[1] ;});
+            randNumbers.push_back(normalized_randn);
+        }
+        else
+        {
+            randNumbers.push_back(unnormalized_randn);
+        }
+       
     }
     
-    double myconstant{-1.0};
-    for(int i = 0; i < nScenarios / 2; i++) //notera att randn är dyrt och vi slipper det här.
+    if(antithetic)
     {
-        vector<double> anti_path(nDates,0.0);
-        vector<double> temp_rand = randNumbers[i];
-        transform(temp_rand.begin(), temp_rand.end(), anti_path.begin(), [&myconstant](double& c){return c*myconstant;});
-        randNumbers.push_back(anti_path);
-        
+        double myconstant{-1.0};
+        for(int i = 0; i < nScenarios / anti; i++) //notera att randn är dyrt och vi slipper det här.
+        {
+            vector<double> anti_path(nDates,0.0);
+            vector<double> temp_rand = randNumbers[i];
+            transform(temp_rand.begin(), temp_rand.end(), anti_path.begin(), [&myconstant](double& c){return c*myconstant;});
+            randNumbers.push_back(anti_path);
+            
+        }
     }
-    
     return randNumbers;
 }
 
 
 void testBSMsimulator()
 {
-    rng("default");
     
-    vector<vector<double>> randNumbers;
+    //vector<vector<double>> randNumbers;
     
 
     Simulator2 simulator;
-    simulator.nScenarios = 10000;
+    simulator.nScenarios = 1000000;
     simulator.dates = {3/12, 6/12, 1, 2, 3, 5, 7, 10, 15, 20};
-    randNumbers = simulator.simulation(simulator.dates);
+    //randNumbers = simulator.simulation(simulator.dates,true,true);
     
     BlackScholesModel m;
     m.volatility = 0.1;
@@ -99,23 +106,37 @@ void testBSMsimulator()
     m.drift = 0.1;
     m.date = 0;
     
-    vector<double> res = simulator.indexSimulation(m, simulator.dates, randNumbers);
-    double a = 5.0;
-    cout<<a<<endl;
+    vector<vector<double>> randNumbers;
+    vector<double> res;
+    
+    vector<vector<double>> randNumbers2;
+    vector<double> res2;
+    
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+    randNumbers = simulator.simulation(simulator.dates,true,true);
+    res = simulator.indexSimulation(m, simulator.dates, randNumbers);
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    
+    chrono::steady_clock::time_point begin2 = chrono::steady_clock::now();
+    randNumbers2 = simulator.simulation(simulator.dates,false,true);
+    res2 = simulator.indexSimulation(m, simulator.dates, randNumbers2);
+    chrono::steady_clock::time_point end2 = chrono::steady_clock::now();
     
 
-   
+    double time1 = (end - begin).count();
+    double time2 = (end2 - begin2).count();
+    //double time_diff = time2-time1;
+    cout<< time1<<endl;
+    cout << time2 <<endl;
+    //cout << time_diff << endl;
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
+    //std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
     
-  
-    //105.1615 vid 1k steg vs 105.011 vid 10 steg, båda fallen 10k scenarios.
-    //Antithetic 104.6027, 104.6027, fick exakt samma nu.
-    //Första metoden var att for loopa och köra n_inv på 1-U, men n_inv kostar nog mycket? Så vi vill istället
-    //Köra n/2 stycken U, och ta fram n_inv på dessa och sen ersätta dessa med negativa värden, verkar bli samma sak
-    //Det som återstår att testa är ifall jag kommer få exakt samma sak om jag inte kör atithetic men kör fler scenarios,
-    //Den borde konvergera till samma värde!
-    //Ska man lägga till antithetic sampling direkt och normaliserings optimering + vektoriseringen???
-    //Sen kommer alla derivat använda samma index-vektor?
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end2 - begin2).count() << "[µs]" << std::endl;
+    //std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
     
+    cout<<"testBSMsimulator is done."<<endl;
+
     
 }
  
--- Simulator2.h
+++ Simulator2.h
@@ -24,13 +24,9 @@ public:
     int nScenarios;
     std::vector<double> dates;
     
-    std::vector<std::vector<double>> simulation(std::vector<double> dates);
+    std::vector<std::vector<double>> simulation(std::vector<double> dates, bool antithetic, bool standardisation);
     std::vector<double> indexSimulation(const BlackScholesModel& bsm, std::vector<double> dates, std::vector<std::vector<double>> randN) const;
     
 };
 
 void testBSMsimulator();
-
-//För en viss uppsättning av bsm, unik T, volatilitet och r, så simulerar vi fram till T, detta kan återanvändas i alla //portföljer som använder samma typ av option??? i nuläget behövde put-option och call-option att köra om samma beräkning// //men om dessa har samma underliggande, så kan man använda exakt samma objekt för simuleringen?
-
-//Till att börja med så simulerar vi för ett visst T, så att en put och call med samma underliggande kan återanvända resultatet.
